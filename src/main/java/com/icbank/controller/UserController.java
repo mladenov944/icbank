0ef49ac9baf8b37807aa0302633dca236d7fba45
Ivelin <Ivelin@DESKTOP-S3N1FPT>
2018-10-30 18:05:55 +0200

    register

--- src/main/java/com/icbank/controller/UserController.java
+++ src/main/java/com/icbank/controller/UserController.java
@@ -2,16 +2,20 @@ package com.icbank.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.icbank.model.User;
+import com.icbank.model.UserDTO;
 import com.icbank.service.UserService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/user-managment")
 public class UserController {
 
@@ -24,6 +28,16 @@ public class UserController {
 			return new ResponseEntity<User>(userService.register(user), HttpStatus.CREATED);
 		} catch (Exception e) {
 			return new ResponseEntity<String>("Error occured while trying to register! " + e.getMessage(),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> login(@RequestBody UserDTO user) {
+		try {
+			return new ResponseEntity<User>(userService.login(user.getUsername(), user.getPassword()), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<String>("Wrong username or Password! " + e.getMessage(),
 					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
--- src/main/java/com/icbank/service/UserService.java
+++ src/main/java/com/icbank/service/UserService.java
@@ -1,27 +1,36 @@
 package com.icbank.service;
 
+import java.math.BigDecimal;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.icbank.model.User;
 
 @Service
+@CrossOrigin
 public class UserService extends BaseService implements UserDetailsService {
 
+	@CrossOrigin
 	public User register(User user) {
 
 		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+		String ibanche = "BG00ICB";
+		int randIban = (int) (Math.random() * 10000 + 137527491);
+		String finalIban = ibanche + randIban;
 
 		User tempUser = new User();
 		tempUser.setFirstName(user.getFirstName());
 		tempUser.setSecondName(user.getSecondName());
 		tempUser.setSurname(user.getSurname());
-		tempUser.setIban(user.getIban());
+		tempUser.setIban(finalIban);
 		tempUser.setEmail(user.getEmail());
-		tempUser.setCash(user.getCash());
+		tempUser.setCash(BigDecimal.valueOf(500));
 		tempUser.setUsername(user.getUsername());
 		tempUser.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		tempUser.setEnabled(true);
@@ -31,7 +40,20 @@ public class UserService extends BaseService implements UserDetailsService {
 
 	}
 
+	@CrossOrigin
+	public User login(String username, String password) {
+
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		String kriptnata = bCryptPasswordEncoder.encode(password);
+		User temp = (User) loadUserByUsername(username);
+		if (bCryptPasswordEncoder.matches(password, kriptnata)) {
+			return temp;
+		}
+		throw new UsernameNotFoundException("Wrong username or password! ");
+	}
+
 	@Override
+	@CrossOrigin
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = getEm().createNamedQuery("user.findUserByUsername", User.class).setParameter("pUsername", username)
 				.getSingleResult();
